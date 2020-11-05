f3b83f4eace28939dfee4ce9f31096dcb188e934
Eduardo Bravo <66187093+EduardoBravoP@users.noreply.github.com>
2020-11-05 12:05:47 -0300

    Add files via upload

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,12 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
-import Transaction from '../models/Transaction';
+/* eslint-disable prettier/prettier */
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import Transaction from '../models/Transaction'
+
+interface Request {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
+}
 
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
@@ -8,9 +15,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type}: Request): Transaction {
+    if(type === 'outcome') {
+      if(!this.transactionsRepository.isValidTransaction({ title, value, type })) {
+        throw new Error('Saldo não disponível');
+      }
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    })
+
+    return transaction
+    }
   }
-}
 
 export default CreateTransactionService;
